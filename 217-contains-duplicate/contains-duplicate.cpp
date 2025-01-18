115e3b37b290dabda387ee150956af41acf5e1aa
Siddharthsinh Rana <rsidsinh12@gmail.com>
2025-01-18 10:25:05 +0530

    Time: 27 ms (75.87%) | Memory: 73.5 MB (47.42%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool containsDuplicate(std::vector<int>& nums) {
+    bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> seen;
         for(int num : nums) {
             if(seen.find(num) != seen.end()) {
