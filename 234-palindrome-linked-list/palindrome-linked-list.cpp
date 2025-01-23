e25a83ba77de073c8ff98888c8af5b9760b45e50
Siddharthsinh Rana <rsidsinh12@gmail.com>
2025-01-23 10:54:27 +0530

    Time: 0 ms (100.00%) | Memory: 118 MB (82.89%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,22 +10,45 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-    vector<int> arr;
-    ListNode* curr = head;
-
-    // Store linked list values in arr
-    while (curr != nullptr) {
-        arr.push_back(curr->val);
-        curr = curr->next;
+    ListNode* reverseList(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* temp;
+        while (curr) {
+            temp = curr->next;
+            curr -> next = prev;
+            prev = curr;
+            curr = temp;
+        }return prev;
     }
+    bool isPalindrome(ListNode* head) {
+        if(!head || !head->next) return true;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next != nullptr && fast->next->next != nullptr){
+            fast= fast->next->next;
+            slow= slow->next;
+        }slow= reverseList(slow->next);
+        while(slow){
+            if(head->val != slow->val) return false;
+            slow =slow->next;
+            head = head->next;
+        }return true;
+    // vector<int> arr;
+    // ListNode* curr = head;
 
-    // Create a copy and reverse it
-    vector<int> revarr = arr;
-    reverse(revarr.begin(), revarr.end());
+    // // Store linked list values in arr
+    // while (curr != nullptr) {
+    //     arr.push_back(curr->val);
+    //     curr = curr->next;
+    // }
 
-    // Compare original and reversed arrays
-    return arr == revarr;
+    // // Create a copy and reverse it
+    // vector<int> revarr = arr;
+    // reverse(revarr.begin(), revarr.end());
+
+    // // Compare original and reversed arrays
+    // return arr == revarr;
         
 
     }
