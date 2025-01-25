b1c594f2e41a459c0ad2c924c57e94133e91d22f
Siddharthsinh Rana <rsidsinh12@gmail.com>
2025-01-25 15:41:57 +0530

    Time: 27 ms (49.11%) | Memory: 85.8 MB (40.44%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,39 +1,39 @@
-#include <bits/stdc++.h>
-using namespace std;
+    #include <bits/stdc++.h>
+    using namespace std;
 
-class Solution {
-public:
-    int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        stack<int> st;
-        vector<int> left(n), right(n);
+    class Solution {
+    public:
+        int largestRectangleArea(vector<int>& heights) {
+            int n = heights.size();
+            stack<int> st;
+            vector<int> left(n), right(n);
+            
+            
+            for (int i = 0; i < n; i++) {
+                while (!st.empty() && heights[st.top()] >= heights[i])
+                    st.pop();
+                left[i] = st.empty() ? 0 : st.top() + 1;
+                st.push(i);
+            }
+            
+            
+            while (!st.empty()) st.pop();
+
         
-        
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && heights[st.top()] >= heights[i])
-                st.pop();
-            left[i] = st.empty() ? 0 : st.top() + 1;
-            st.push(i);
+            for (int i = n - 1; i >= 0; i--) {
+                while (!st.empty() && heights[st.top()] >= heights[i])
+                    st.pop();
+                right[i] = st.empty() ? n - 1 : st.top() - 1;
+                st.push(i);
+            }
+
+            
+            int maxArea = 0;
+            for (int i = 0; i < n; i++) {
+                int width = right[i] - left[i] + 1;
+                maxArea = max(maxArea, heights[i] * width);
+            }
+
+            return maxArea;
         }
-        
-        
-        while (!st.empty()) st.pop();
-
-       
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && heights[st.top()] >= heights[i])
-                st.pop();
-            right[i] = st.empty() ? n - 1 : st.top() - 1;
-            st.push(i);
-        }
-
-        
-        int maxArea = 0;
-        for (int i = 0; i < n; i++) {
-            int width = right[i] - left[i] + 1;
-            maxArea = max(maxArea, heights[i] * width);
-        }
-
-        return maxArea;
-    }
-};
+    };
