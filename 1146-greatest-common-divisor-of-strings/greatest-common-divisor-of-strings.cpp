ba2662b20a6af402903f3cc1cceb48814030f08f
Siddharthsinh Rana <rsidsinh12@gmail.com>
2025-02-05 10:47:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (32.09%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
     if (str1 + str2 != str2 + str1)  
-        return "";  // If concatenation order differs, no common divisor
+        return "";  
     
         int gcd_length = gcd(str1.size(), str2.size());  
-        return str1.substr(0, gcd_length);  // Return prefix of gcd length
+        return str1.substr(0, gcd_length);  
     }
 };
